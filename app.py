d295105f8a136791b7efa1f5890602572a75ec23
nico <nicolas.p.suarez@gmail.com>
2018-10-22 13:33:08 -0300

    Add account model view

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from config import Config
 from flask import Flask
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,9 +11,12 @@ db = SQLAlchemy(app)
 # Models can only be imported after db is declared.
 from models.account import Account
 
+# Import views.
+from views.accountview import AccountView
+
 # Initialize the admin interface.
 admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
-admin.add_view(ModelView(Account, db.session))
+admin.add_view(AccountView(Account, db.session))
 
 @app.route("/")
 def index():
---
+++ views/accountview.py
@@ -0,0 +1,10 @@
+from flask_admin.contrib.sqla import ModelView
+
+
+class AccountView(ModelView):
+    """
+    Model view class for account model.
+    """
+
+    # Remove fields from the create and edit forms.
+    form_excluded_columns = ['status', 'status_updated_at']
