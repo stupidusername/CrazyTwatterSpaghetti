260bc145e43b0c9daab484814bb19b3648d7e288
nico <nicolas.p.suarez@gmail.com>
2018-10-22 15:05:38 -0300

    Add account required fields

--- models/account.py
+++ models/account.py
@@ -7,9 +7,9 @@ class Account(db.Model):
     """
 
     id = db.Column('id', db.Integer, primary_key=True)
-    screen_name = db.Column('screen_name', db.String())
-    email = db.Column('email', db.String())
-    password = db.Column('password', db.String())
-    phone_number = db.Column('phone_number', db.String())
+    screen_name = db.Column('screen_name', db.String(), nullable=False)
+    email = db.Column('email', db.String(), nullable=False)
+    password = db.Column('password', db.String(), nullable=False)
+    phone_number = db.Column('phone_number', db.String(), nullable=False)
     status = db.Column('status', db.String())
     status_updated_at = db.Column('status_updated_at', db.DateTime())
