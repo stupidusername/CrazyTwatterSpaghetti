753a87acc4540127c4f941a8bf2134e6c8410796
nico <nicolas.p.suarez@gmail.com>
2018-10-25 02:15:00 -0300

    Add update_status method to account model

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # CrazyTwatterSpaghetti
 
 ## Requirements
+
 - Python >= 3.6 (Tested on 3.6.6).
 - Pip packages listed on `requirements.txt`.
 - A RDBMS supported by SQLAlchemy. See [SQLAlchemy 1.2 Documentation > Dialects](https://docs.sqlalchemy.org/en/latest/dialects/index.html).
 
 ## Installation
+
 - Create a `config.ini`. Change the database DSN and the secret key used for cookies. Use `config-example.ini` as reference.
 - Apply DB migrations: `$ alembic upgrade head`.
 
 ## Usage
+
 Run the flask application defined in `app.py`.
 
+Note: Dates and times are displayed in UTC.
+
 ## Accounts CSV format
+
 `<screen_name>, <email>, <password>, <phone_number>`
 
 ## API endpoints
+
 - `/api/poll-info/<tweet_id>`
 
   Get information about a twitter poll given its tweet id.
--- models/account.py
+++ models/account.py
@@ -1,23 +1,25 @@
 from app import db
+from datetime import datetime
 
 
 class Account(db.Model):
     """
     Account model.
 
-    Status will be blank for those account which were not tested or were not
-    able to complete a login process.
+    Status will be blank for those account which were not tested.
 
     :const str LOGGED_IN: Login was successful.
     :const str WRONG_CREDENTIALS: Incorrect screen name or password.
     :const str SUSPENDED: Suspended account.
     :const str UNCONFIRMED_ACCESS: A manual confirmation is needed.
+    :const str UNDETERMINED: Unable to complete the login process.
     """
 
     STATUS_LOGGED_IN = 'logged_in'
     STATUS_WRONG_CREDENTIALS = 'wrong_credentials'
     STATUS_SUSPENDED = 'suspended'
     STATUS_UNCONFIRMED_ACCESS = 'unconfirmed_acess'
+    STATUS_UNDETERMINED = 'undetermined'
 
     id = db.Column('id', db.Integer, primary_key=True)
     screen_name = \
@@ -27,3 +29,13 @@ class Account(db.Model):
     phone_number = db.Column('phone_number', db.String(), nullable=False)
     status = db.Column('status', db.String())
     status_updated_at = db.Column('status_updated_at', db.DateTime())
+
+    def update_status(self, status: str):
+        """
+        Update status and status_updated_at fields.
+
+        :param str status: New status.
+        """
+        self.status = status
+        self.status_updated_at = datetime.utcnow()
+        db.session.commit()
