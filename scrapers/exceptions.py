15d509c8b3f9d3d1fd68b5d115ebd89be54a7934
nico <nicolas.p.suarez@gmail.com>
2018-10-26 18:51:03 -0300

    Add TwitterVoteException

--- scrapers/exceptions.py
+++ scrapers/exceptions.py
@@ -3,3 +3,10 @@ class TwitterScrapingException(Exception):
     An exception raised when an error occurs during scraping.
     """
     pass
+
+
+class TwitterVoteException(TwitterScrapingException):
+    """
+    An exception raised during a poll vote.
+    """
+    pass
