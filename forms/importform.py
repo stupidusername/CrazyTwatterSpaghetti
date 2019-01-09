2fad3497926b95a73ba34cf156f249f064044f42
nico <nicolas.p.suarez@gmail.com>
2019-01-09 02:32:15 -0300

    Avoid duplicate key error when importing accounts

--- forms/importform.py
+++ forms/importform.py
@@ -1,14 +1,18 @@
 import _thread
-from createapp import db
+from createapp import create_app, db
 from csv import DictReader
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField, FileRequired
 import io
+import logging
+from logging.handlers import RotatingFileHandler
 from models.account import Account
 import random
 from scrapers.twitterlogin import TwitterLogin
 from scrapers.twitterpasswordupdate import TwitterPasswordUpdate
+from sqlalchemy import or_
 import string
+from typing import List
 from wtforms import BooleanField
 
 
@@ -42,13 +46,24 @@ class ImportForm(FlaskForm):
                     'proxy'
                 )
             )
+            # Avoid accounts that were already loaded.
+            rows = []
+            for row in reader:
+                account = Account.query.filter(
+                    or_(
+                        Account.screen_name == row['screen_name'],
+                        Account.email == row['screen_name']
+                    )
+                ).first()
+                if not account:
+                    rows.append(row)
             if self.change_passwords.data:
                 # Change passwords if needed.
-                _thread.start_new_thread(self._change_passwords, (reader,))
+                _thread.start_new_thread(self._change_passwords, (rows,))
             else:
                 # Save the accounts otherwise.
                 db.session.bulk_save_objects(
-                    [Account(**row) for row in reader]
+                    [Account(**row) for row in rows]
                 )
                 db.session.commit()
         else:
@@ -56,16 +71,29 @@ class ImportForm(FlaskForm):
                 'The uploaded file is an empty instance of FileStorage.'
             )
 
-    def _change_passwords(self, reader: DictReader):
+    def _change_passwords(self, rows: List[dict]):
         """
         Change the password of the accounts that are loaded in the `dict`
         param. Also save the accounts to the DB.
 
-        :param DictReader reader: A reader that contains the information of the
-            accounts.
+        :param [dict] rows: A list containing the information of the accounts.
         """
+        # Push an app context.
+        app = create_app()
+        app.app_context().push()
+        # Create a rotating log for errors.
+        logger = logging.getLogger()
+        logger.setLevel(logging.ERROR)
+        handler = RotatingFileHandler(
+            'logs/password_update.log',
+            maxBytes=65536,
+            backupCount=5
+        )
+        logger.addHandler(handler)
+        # Also send errors to stderr.
+        logger.addHandler(logging.lastResort)
         # Each row represents the information of one account.
-        for row in reader:
+        for row in rows:
             # Create the account model.
             account = Account(**row)
             # Generate the new password.
@@ -78,7 +106,7 @@ class ImportForm(FlaskForm):
                 account.password = new_password
             except Exception as e:
                 # There was an error during the password update.
-                app.logger.error(
+                logger.error(
                     '"%s" password update failed with exception: %s',
                     account.screen_name,
                     e
